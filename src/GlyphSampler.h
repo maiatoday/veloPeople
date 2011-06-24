687ea97dae9628e39f590ba7af7b3869b5d683fe
maiatoday <maiatoday@gmail.com>
2011-06-24 15:21:24 +0200

    GlyphSampler class

---
+++ src/GlyphSampler.h
@@ -0,0 +1,22 @@
+#ifndef GLYPHSAMPLER_H
+#define GLYPHSAMPLER_H
+
+#include <ofImage.h>
+
+#include <vector>
+
+class GlyphSampler
+{
+    public:
+        GlyphSampler(std::string _basefilename);
+        virtual ~GlyphSampler();
+        ofImage getSampleGlyph();
+    protected:
+    bool isImageFile(std::string _filename);
+    private:
+    std::vector<ofImage*> goodGlyphs;
+    ofImage defaultGlyph;
+    unsigned char* pPixels;
+};
+
+#endif // GLYPHSAMPLER_H
