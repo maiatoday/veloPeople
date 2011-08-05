e1b8188b728e5fe1ee5e95279e64e1369be90dc8
maiatoday <maiatoday@gmail.com>
2011-08-05 07:58:15 +0200

    fix small bug in start script going to wrong directory

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #autostart script which will work with lubuntu install
-cd /usr/share/lumoPeople/boodler
+cd /usr/share/forkmerge/boodler
 boodler --output pulse --listen --extern theagents com.example.theagents/Example &
 cd /usr/share/forkmerge/bin
 ./forkmerge &
