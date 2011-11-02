750709993f66ac0fd131ba2401da81a7dd95750b
maiatoday <maiatoday@gmail.com>
2011-11-02 11:27:25 +0200

    scripts for launnchers, specific to my install but can be modified

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 #autostart script which will work with lubuntu install
 #cd /usr/share/hashhatch/boodler
 #boodler --output pulse --listen --extern theagents com.example.theagents/Example &
-cd /usr/share/initbefore/bin
-./initbefore &
+cd /home/maia/oF/openFrameworks/apps/myApps/initbefore/bin
+./initbefore
