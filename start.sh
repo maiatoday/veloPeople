cc185e463cbd61a98551579ed8ce23b8d2f30238
maiatoday <maiatoday@gmail.com>
2011-11-03 07:52:51 +0200

    make icons work for install disk

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 #autostart script which will work with lubuntu install
-cd /home/maia/oF/openFrameworks/apps/myApps/lumoPeople/boodler
+#cd /home/maia/oF/openFrameworks/apps/myApps/lumoPeople/boodler
+cd /usr/share/lumoPeople/boodler
 ./run&
-cd /home/maia/oF/openFrameworks/apps/myApps/lumoPeople/bin
+#cd /home/maia/oF/openFrameworks/apps/myApps/lumoPeople/bin
+cd /usr/share/lumoPeople/bin
 ./lumoPeople 
