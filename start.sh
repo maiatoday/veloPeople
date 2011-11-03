ebfdca07bc2565824d530688e40559eb228182ea
maiatoday <maiatoday@gmail.com>
2011-11-03 07:51:01 +0200

    make icons work for install disk

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 #autostart script which will work with lubuntu install
 #cd /usr/share/hashhatch/boodler
 #boodler --output pulse --listen --extern theagents com.example.theagents/Example &
-cd /home/maia/oF/openFrameworks/apps/myApps/veloPeople/bin
+#cd /home/maia/oF/openFrameworks/apps/myApps/veloPeople/bin
+cd /usr/share/veloPeople/bin
 ./veloPeople
