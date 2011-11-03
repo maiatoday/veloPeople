a74e5d992f045a11fae1d55da8805ee1094a0afe
maiatoday <maiatoday@gmail.com>
2011-11-03 07:54:57 +0200

    make icons work for install disk

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 #autostart script which will work with lubuntu install
 #cd /home/maia/oF/openFrameworks/apps/myApps/forkmerge/boodler
 #boodler --output pulse --listen --extern theagents com.example.theagents/Example &
-cd /home/maia/oF/openFrameworks/apps/myApps/forkmerge/bin
+#cd /home/maia/oF/openFrameworks/apps/myApps/forkmerge/bin
+cd /usr/share/forkmerge/bin
 ./forkmerge
