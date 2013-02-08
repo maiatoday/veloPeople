ffd35392b508155f006a530a95926de84361542d
Maia Grotepass <maiatoday@gmail.com>
2013-02-08 09:42:16 +0200

    Update to new oF. On 64bit install does not detect.

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 #autostart script which will work with lubuntu install
 #cd /home/maia/oF/openFrameworks/apps/myApps/lumoPeople/boodler
-cd /usr/share/lumoPeople/boodler
+cd /home/maia/oF/of_v0071_linux64_release/apps/myApps/lumoPeople/boodler
 ./run&
 #cd /home/maia/oF/openFrameworks/apps/myApps/lumoPeople/bin
-cd /usr/share/lumoPeople/bin
-./lumoPeople 
+cd /home/maia/oF/of_v0071_linux64_release/apps/myApps/lumoPeople/bin
+./lumoPeople_debug 
